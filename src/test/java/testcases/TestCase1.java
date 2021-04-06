39fb77b88405a7633d2f0023983cf93475047024
muleraja01 <69101219+muleraja01@users.noreply.github.com>
2021-04-06 16:58:04 +0530

    Adding TestNG Files

--- src/test/java/testcases/TestCase1.java
+++ src/test/java/testcases/TestCase1.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class TestCase1 {
+public class TestCase1 extends BaseTest{
 
 	@BeforeTest
 	public void creatingDBConn() {
@@ -24,12 +24,12 @@ public class TestCase1 {
 	public void closeBrowser() {
 		System.out.println("Closing the browser");
 	}
-	@Test(priority = 1)
+	@Test(priority = 1,groups="functional")
 	public void doUserReg() {
 		System.out.println("Executing User Reg Test");
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2,groups="functional")
 	public void doLogin() {
 		System.out.println("Executing Login Test");
 	}
--- src/test/java/testcases/TestCase2.java
+++ src/test/java/testcases/TestCase2.java
@@ -2,18 +2,23 @@ package testcases;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
-public class TestCase2 {
+public class TestCase2 extends BaseTest{
 
-	@Test
+	@Test(groups="Smoke")
 	public void validateTitle() {
 		String expected_Title="Yahoo.com";
 		String actual_Title="Google.com";
+		SoftAssert softAssert = new SoftAssert();
 /*		if(expected_Title.equals(actual_Title)) {
 			System.out.println("Test Case Pass");
 		}else {
 			System.out.println("Test case Fail");
 		}*/
-		Assert.assertEquals(actual_Title, expected_Title);
+		softAssert.assertEquals(actual_Title, expected_Title);
+		softAssert.assertTrue(false, "Title Didnt Match");
+		System.out.println("Ending...");
+		softAssert.assertAll();
 	}
 }
